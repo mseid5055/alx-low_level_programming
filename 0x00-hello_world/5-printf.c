2f94bbf65ceaea91f306723d0f89cfe05c1f7aeb
mseid5055 <mseid5055@gmail.com>
2023-02-28 04:23:09 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * return: Always 0 (success)
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf(''with proper grammar, but the outcome is a piece of art,\n'');
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
